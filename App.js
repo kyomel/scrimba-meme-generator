5ca3ffb27ed84207e9603545564abbded0688978
kyomel <michaellapandio04@gmail.com>
2024-10-03 19:48:20 +0700

    add data memes for response button

--- App.js
+++ App.js
@@ -2,9 +2,6 @@ import React from "react"
 import Header from "./components/Header"
 import Meme from "./components/Meme"
 
-/**
- * Challenge: Build the Header component
- */
 export default function App() {
     return (
         <div>
--- components/Meme.js
+++ components/Meme.js
@@ -1,12 +1,29 @@
 import React from "react"
+import memesData from "../memesData"
 
 export default function Meme() {
+  let url
+  function getMemeImage() {
+    const memeArr = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memeArr.length)
+    url = memeArr[randomNumber].url
+    console.log(url)
+  }
   return (
     <main>
+      <p>{url}</p>
       <form className="form">
-        <input className="form--input" type="text" placeholder="Top text"/>
-        <input className="form--input" type="text" placeholder="Bottom text"/>
-        <button className="form--button">Get a new meme image 🖼</button>
+        <div>
+          <label htmlFor="top-text">Top Text
+            <input id="top-text" className="form--input" type="text" placeholder="Shut up"/>
+          </label>
+        </div>
+        <div>
+          <label>Bottom Text
+            <input id="" className="form--input" type="text" placeholder="and take my money"/>
+          </label>  
+        </div>
+        <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
       </form>
     </main>
   )
